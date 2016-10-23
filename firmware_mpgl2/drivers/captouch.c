fca7781ad1346b5a3b3ad5b18c2d503770b45ef8
admilden <admilden@ucalgary.ca>
2016-10-23 11:31:54 -0600

    Fixing additional issues
    Accidentally removed or left too many things. Additional cleanup. Also
    removed ant_api files. It will now compile without error, but also
    doesn't do anything.

--- firmware_mpgl2/drivers/captouch.c
+++ firmware_mpgl2/drivers/captouch.c
@@ -8,9 +8,7 @@ and any future buttons/sliders that may be added.
 Restarted by Alex Mildenberger
 October 23, 2016
 
+***********************************************************************************************************************/
+
 #include "configuration.h"
-#include "libqtouch.h"
 
-
-#define GET_SENSOR_STATE(SENSOR_NUMBER) (qt_measure_data.qt_touch_status.sensor_states[(SENSOR_NUMBER/8)] & (1 << (SENSOR_NUMBER % 8)))
-#define GET_ROTOR_SLIDER_POSITION(ROTOR_SLIDER_NUMBER) qt_measure_data.qt_touch_status.rotor_slider_values[ROTOR_SLIDER_NUMBER]
